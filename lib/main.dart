2ff7dc8310f3a4b3c8a2bc2d56c4b952fac0a58f
afifsofimahdy <afifsmahd86@gmail.com>
2022-06-04 01:12:08 +0700

    FCM & LOCAL NOTIFICATION

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,41 @@ import 'dart:async';
 
 import 'package:cnn_app/routes/routes.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+const AndroidNotificationChannel channel = AndroidNotificationChannel(
+    'high_importance_channel', // id
+    'High Importance Notifications', // title
+    description: 'This channel is used for important notifications.', 
+    importance: Importance.high,
+    playSound: true
+  );
+
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
+
+Future<void> firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+  debugPrint('A bg message just showed up :  ${message.messageId}');
+}
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  FirebaseMessaging.onBackgroundMessage(firebaseMessagingBackgroundHandler);
+
+  await flutterLocalNotificationsPlugin
+      .resolvePlatformSpecificImplementation<AndroidFlutterLocalNotificationsPlugin>()
+      ?.createNotificationChannel(channel);
+
+  await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
+    alert: true,
+    badge: true,
+    sound: true,
+  );
   runApp(const MaterialApp(
     title: "CNN INDONESIA",
 
--- lib/views/main_page/main_page.dart
+++ lib/views/main_page/main_page.dart
@@ -1,9 +1,12 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
+import 'package:cnn_app/main.dart';
 import 'package:cnn_app/views/home_page/home_page.dart';
 import 'package:cnn_app/views/profile_page/profile_page.dart';
 import 'package:cnn_app/views/tv_page/tv_page.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class MainPage extends StatefulWidget {
@@ -39,6 +42,48 @@ class _MainPageState extends State<MainPage> with SingleTickerProviderStateMixin
          loadData = false;
        });
      });
+
+     FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      RemoteNotification? notification = message.notification;
+      AndroidNotification? android = message.notification?.android;
+      if (notification != null && android != null) {
+        flutterLocalNotificationsPlugin.show(
+            notification.hashCode,
+            notification.title,
+            notification.body,
+            NotificationDetails(
+              android: AndroidNotificationDetails(
+                channel.id,
+                channel.name,
+                channelDescription:channel.description,
+                color: Colors.blue,
+                playSound: true,
+                icon: '@mipmap/ic_launcher',
+              ),
+            ));
+      }
+    });
+
+    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+      debugPrint('A new onMessageOpenedApp event was published!');
+      RemoteNotification? notification = message.notification;
+      AndroidNotification? android = message.notification?.android;
+      if (notification != null && android != null) {
+        showDialog(
+            context: context,
+            builder: (_) {
+              return AlertDialog(
+                title: Text(notification.title!),
+                content: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [Text(notification.body!)],
+                  ),
+                ),
+              );
+            });
+      }
+    });
   }
 
   Future getDataUser() async {
@@ -52,7 +97,18 @@ class _MainPageState extends State<MainPage> with SingleTickerProviderStateMixin
   }
 
 
-
+    void showNotification() {
+    flutterLocalNotificationsPlugin.show(
+      0,
+      "Apa gitu",
+      "Hayo ngapain ?",
+      NotificationDetails(
+        android: AndroidNotificationDetails(channel.id, channel.name, channelDescription: channel.description,
+            importance: Importance.high,
+            color: Colors.blue,
+            playSound: true,
+            icon: '@mipmap/ic_launcher')));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -78,6 +134,9 @@ class _MainPageState extends State<MainPage> with SingleTickerProviderStateMixin
         controller: _tabController,
         tabs: list, 
         indicatorColor: Colors.grey.withOpacity(0.1),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: ()=> showNotification(),
       ),
     );
   }
